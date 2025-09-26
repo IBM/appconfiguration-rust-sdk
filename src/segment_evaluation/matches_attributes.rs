f6a4f046f58f0b7274af3b0cafab41e2c449096e
Javier G. Sogo <jgsogo@gmail.com>
2025-09-26 12:00:24 +0200

    refact: Clarify `belong_to_segment` algorithm (#118)
    * Introduces a `MatchesEntity` trait that is implemented for `Segment`
    and `Rule` so we can isolate the logic that applies to each one of them
    when matching an entity.
    
    ---------
    
    Signed-off-by: Javier G. Sogo <jgsogo@gmail.com>
    Signed-off-by: Rainer Schoenberger <rschoe@de.ibm.com>
    Co-authored-by: Travis <rschoe@de.ibm.com>
    Co-authored-by: Rainer Schoenberger <36071440+rainerschoe@users.noreply.github.com>
    Signed-off-by: Travis <rschoe@de.ibm.com>

---
+++ src/segment_evaluation/matches_attributes.rs
@@ -0,0 +1,78 @@
+// (C) Copyright IBM Corp. 2025.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::collections::HashMap;
+
+use super::errors::CheckOperatorErrorDetail;
+use crate::network::serialization::{Rule, Segment};
+use crate::segment_evaluation::errors::SegmentEvaluationError;
+use crate::segment_evaluation::rule_operator::RuleOperator;
+use crate::Value;
+
+pub(crate) trait MatchesAttributes {
+    type Error;
+
+    fn matches_attributes(
+        &self,
+        attributes: &HashMap<String, Value>,
+    ) -> std::result::Result<bool, Self::Error>;
+}
+
+impl MatchesAttributes for Segment {
+    type Error = SegmentEvaluationError;
+
+    /// A [`Segment`] matches attributes iif:
+    /// * ALL the rules match the attributes
+    fn matches_attributes(
+        &self,
+        attributes: &HashMap<String, Value>,
+    ) -> std::result::Result<bool, Self::Error> {
+        self.rules
+            .iter()
+            .map(|rule| {
+                rule.matches_attributes(attributes)
+                    .map_err(|(e, rule_value)| (e, self, rule, rule_value).into())
+            })
+            .collect::<std::result::Result<Vec<bool>, _>>()
+            .map(|v| v.iter().all(|&x| x))
+    }
+}
+
+impl MatchesAttributes for Rule {
+    type Error = (CheckOperatorErrorDetail, String);
+
+    /// A [`Rule`] matches attributes iif:
+    /// * the attributes contain the requested attribute, AND
+    /// * the attribute satisfies ANY of the rule values.
+    ///
+    /// TODO: What if rules.values is empty? Now it returns false
+    fn matches_attributes(
+        &self,
+        attributes: &HashMap<String, Value>,
+    ) -> std::result::Result<bool, Self::Error> {
+        attributes
+            .get(&self.attribute_name)
+            .map_or(Ok(false), |attr_value| {
+                self.values
+                    .iter()
+                    .map(|value| {
+                        attr_value
+                            .operate(&self.operator, value)
+                            .map_err(|e| (e, value.to_owned()))
+                    })
+                    .collect::<std::result::Result<Vec<bool>, _>>()
+                    .map(|v| v.iter().any(|&x| x))
+            })
+    }
+}
